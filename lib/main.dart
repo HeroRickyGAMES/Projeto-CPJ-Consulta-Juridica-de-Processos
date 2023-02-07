1b42d6ac7c7d46eb46680fae7f1a9a1a3fc935b9
herorickygames <ricojn9@gmail.com>
2023-02-07 11:40:36 -0300

    Melhorias na lsita

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ import './Tribunais.dart';
 import './Noticias.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 //Programado por HeroRickyGames
 
